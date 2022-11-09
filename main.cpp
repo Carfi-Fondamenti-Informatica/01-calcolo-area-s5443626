62ad3d2aa208037bf4e6271e471b7627e2ec7d25
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-11-09 13:21:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+float a=0, b=0, c=0;
+  cin >> a >> b >> c;
+  cout << ((a*b)/2) << endl;
+  cout << (a*a) << endl;
+  cout << (a*b) << endl;
+  cout << (((a+b)*c)/2) << endl;
+  return0;
 }
